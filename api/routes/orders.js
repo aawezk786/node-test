14db2662f33dd0f4d888666872d68a65792b4622
Rifa-K-Malik <malikrif07@gmail.com>
2020-12-05 13:30:34 +0530

    Managing orders with mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -78,6 +78,11 @@ router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
         .exec()
         .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "Order not found"
+                });
+            }
             res.status(200).json({
                 order: order,
                 request: {
